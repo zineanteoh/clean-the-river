54a6e49c7e5d98612cf3ba12bdb13faa0701b3f2
Zi Nean Teoh <zinean00@gmail.com>
2021-08-02 16:08:59 +0800

    Added mute music feature

--- src/audio.js
+++ src/audio.js
@@ -2,14 +2,47 @@
 
 const url = "../audio/music.wav";
 let audioElement;
+const controlMusic = document.querySelector(".control-music");
+let isPlaying = false;
+let isMute = false;
 
 function loadAudio() {
   audioElement = new Audio(url);
   audioElement.loop = true;
+  document.querySelector(".music").addEventListener("click", function () {
+    if (isPlaying) {
+      // Mute music
+      isMute = true;
+      stopAudio();
+      controlMusic.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    } else {
+      // Play music
+      isMute = false;
+      playAudio();
+      controlMusic.innerHTML = '<i class="fas fa-volume-up"></i>';
+    }
+    isPlaying = !isPlaying;
+  });
 }
 
 function playAudio(url) {
-  audioElement.play();
+  if (!isMute) {
+    let playPromise = audioElement.play();
+    if (playPromise !== undefined) {
+      playPromise
+        .then(function () {
+          // Automatic playback started!
+          isPlaying = true;
+          controlMusic.innerHTML = '<i class="fas fa-volume-up"></i>';
+        })
+        .catch(function (error) {
+          isPlaying = false;
+          // Automatic playback failed.
+          // Show a UI element to let the user manually start playback.
+          controlMusic.innerHTML = '<i class="fas fa-volume-mute"></i>';
+        });
+    }
+  }
 }
 
 function stopAudio() {
